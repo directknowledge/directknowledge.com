9a72648aaf3e1758e355eb3317a658ae4f6d94c1
= <=>
2023-01-24 20:23:24 -0600

    frontpages

--- _webtools/render-books.py
+++ _webtools/render-books.py
@@ -3,6 +3,6 @@ import os
 os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__basicsettheory")
 os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__learningnumbertheory")
 os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__learninglinearalgebra")
-os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__exploringcalculus")
+os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__singlevariablecalculus")
 os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__multivariablecalculus")
 
