091f4d3679ca5af429336e23a0734e537e92b0db
= <=>
2023-01-30 15:27:41 -0600

    blog

---
+++ _webtools/render.py
@@ -0,0 +1,9 @@
+import os
+
+os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__basicsettheory")
+os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__learningnumbertheory")
+os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__learninglinearalgebra")
+os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__singlevariablecalculus")
+os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/__multivariablecalculus")
+os.system("quarto render /Users/davidsmith/Documents/Sites/directknowledge.com/blog")
+
